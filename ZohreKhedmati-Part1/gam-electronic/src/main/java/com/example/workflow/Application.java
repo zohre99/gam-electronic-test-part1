310e91599b677baa6328fb59364f4503a90726af
zohre99 <47241298+zohre99@users.noreply.github.com>
2022-01-17 20:47:25 +0330

    test-jenkins-job-2

--- ZohreKhedmati-Part1/gam-electronic/src/main/java/com/example/workflow/Application.java
+++ ZohreKhedmati-Part1/gam-electronic/src/main/java/com/example/workflow/Application.java
@@ -3,7 +3,6 @@ package com.example.workflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Application {
 
